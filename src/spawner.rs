0a542738d78352a938a41fa0198a4acd1785cc3c
johndpeek <john.d.peek@gmail.com>
2025-03-12 13:08:28 -0400

    fov implementation

--- src/spawner.rs
+++ src/spawner.rs
@@ -8,7 +8,8 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
             color: ColorPair::new(WHITE, BLACK),
             glyph: to_cp437('@'),
         },
-        Health{current: 20, max: 20}
+        Health{current: 20, max: 20},
+        FieldOfView::new(8)
     ));
 }
 
@@ -31,7 +32,8 @@ pub fn spawn_monster(
             },
             ChasingPlayer{},
             Health{current: hp, max: hp},
-            Name(name)
+            Name(name),
+            FieldOfView::new(6)
         )
     );
 }
